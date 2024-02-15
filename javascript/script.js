aff0e60f6e317d4ec2e465ce93f480354d421c60
kamal <kamade123@gmail.com>
2024-02-15 18:32:49 +0100

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,90 @@
+// this is a javascript comment
+// javascript console - Hello World
+// console.log("My name is sunkanmi")
+// console.log("My name is samson")
+
+// Data types in javascript
+
+// Strings
+let str = "this is a string";
+let str2 = 'this is a string';
+
+// Integers - Integers are numbers
+let int = 7;
+
+// Booleans - Booleans are basicaly true/false
+let rich = true
+let broke = false
+
+
+// Floats - This are decimal numbers
+let cgpa = 3.50
+
+// Arrays - is a way of storing multiple values in javascript
+let colors = ['blue', 'red', 'yellow']
+let food = ['rice', 'beans', 'yam', 'bread']
+// index      0        1       2       3
+
+// console.log(colors[3])
+
+
+// array length - to check the number of items in an array.
+console.log(colors.length)
+// console.log(food.length)
+
+//
+console.log(colors)
+// array push - to add an item to the array
+colors.push('green')
+colors.push('green')
+colors.push('green')
+colors.push('green')
+colors.push('green')
+colors.push('green')
+console.log(colors)
+console.log(colors.length)
+
+// array pop - to remove the last item in an array
+colors.pop()
+
+
+// array reverse - reverses the elements in the array
+colors.reverse()
+console.log(colors)
+console.log(colors.length)
+
+console.log(colors)
+console.log(colors.length)
+
+// array shift - removes the first element in an array
+colors.shift()
+console.log(colors)
+console.log(colors.length)
+
+// console.log(food)
+
+
+// Objects - is also a way of storing multiple values, but in a key, value pair approach 
+const student = {name: "samson", age: 5, color: 'blue', gender: 'male' }
+
+
+// undefined & null
+
+// Variables
+// variable is a way of storin data in javascript
+
+// var name = 'sunkanmi'  // for global variables
+let name2 = 'samson' // for local/enviromrnt variables
+name2 = 'Mr. Olagoke'
+console.log(name2)
+const color = 'blue' // constanat variables
+// color = 'red'
+
+
+// operators in javascript
+// addition
+
+console.log(2 + 2)
+console.log(2 * 2)
+console.log(8 - 2)
+console.log(4 / 2)
